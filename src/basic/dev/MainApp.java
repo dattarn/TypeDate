754b094d43af4ec83719e27c1e0a6745ea731983
dattarn <86503920+dattarn@users.noreply.github.com>
2021-09-29 16:22:21 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,49 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   String fullName = "Tran Van Dat";
+   int masv = 123456;
+   int cmnd = 123456789;
+   String tenLop = "21CDTH41";
+   String tenTruong = "cao dang cong nghiep";
+   byte diemTrungbinh = 8;
+   
+   System.out.println("Ho va ten: " + fullName);
+   System.out.println("Ma sinh vien: " + masv);
+   System.out.println("Chung minh nhan dan: " + cmnd);
+   System.out.println("Ten lop: " + tenLop);
+   System.out.println("Ten truong: " + tenTruong);
+   System.out.println("Diem trung binh: " + diemTrungbinh);
+   
+   
+    String diachi = "8/3/254";
+    String sdt="0963890060";
+	float HESOLUONG = 1.4f;
+	
+	System.out.println("Ho va ten:" + fullName );
+	System.out.println("Dia chi: " + diachi);
+	System.out.println("So dien thoai: " + sdt );
+	System.out.println("He so luong: x" + HESOLUONG);
+	
+	
+	String tenXe = "Honda Civic";
+	String hangXe = "Honda";
+	int gia = 700000000;
+	String mau = "White";
+	
+	System.out.println("Ten xe: " + tenXe);
+	System.out.println("Hang: " + hangXe);
+	System.out.println("Gia: " + gia + "VND");
+	System.out.println("Mau son: " + mau);
+	}
+	
+	
+		
+		
+	
+	
+
+}
